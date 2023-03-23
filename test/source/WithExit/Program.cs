06f04753b88e10e9c0d149e06424ed3718cfbcba
Kadir Yamamoto <me@yamakadi.com>
2023-03-23 22:26:18 +0900

    add source files for test assemblies

---
+++ test/source/WithExit/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Reflection;
+
+namespace WithExit
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine($"[*] Hello World from `{Assembly.GetExecutingAssembly().FullName}`!");
+            Console.WriteLine("[*] I very much hope to live past `System.Environment.Exit`!");
+            Console.WriteLine();
+
+            Environment.Exit(-1);
+            
+            Console.WriteLine("[!] I am still alive!");
+            Console.WriteLine();
+        }
+    }
+}
